fb625c4a9430c6120c8a780d53a5bc88e2045828
mdhamine <143548635+mdhamine@users.noreply.github.com>
2023-12-16 13:09:37 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   env: {
     DB_URL:
       // "mongodb+srv://<rabil>:<rabil>@cluster0.wk291zy.mongodb.net/?retryWrites=true&w=majority",
-      "mongodb+srv://<rabil>:<rabil>@cluster0.wk291zy.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://rabil:rabil@cluster0.wk291zy.mongodb.net/?retryWrites=true&w=majority",
     SENDER_PASSWORD: "wuip jfxb bmgh twcv",
     SENDER_EMAIL: "cyn.webservices@gmail.com",
     RECEIVER_EMAIL: "shopcandy2002@gmail.com",
